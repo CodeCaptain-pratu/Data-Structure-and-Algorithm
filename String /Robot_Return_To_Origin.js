1852308089c8ba96c69833a9e03cfde6fac8d694
Pratiksha Kumbhar <pratikshakumbhar0212@gmail.com>
2026-04-05 23:18:23 +0530

    Added function of Robot Return to Origin
    Added function of Robot Return to Origin

---
+++ String /Robot_Return_To_Origin.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let x=0, y=0;
+    for(let move of moves){
+        if(move==='U'){
+            y++;
+        }else if(move==='D'){
+            y--;
+        }else if(move==='L'){
+            x--;
+        }else{
+            x++;
+        }
+    }
+    return x===0 && y===0;
+};
