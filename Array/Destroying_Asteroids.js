59dec194e6b98a44ad1c5177d94757de2ce69bea
Pratiksha Kumbhar <pratikshakumbhar0212@gmail.com>
2026-05-31 18:53:58 +0530

    Added function of Destroying Asteroids
    Added function of Destroying Asteroids using Sorting Approach

---
+++ Array/Destroying_Asteroids.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} mass
+ * @param {number[]} asteroids
+ * @return {boolean}
+ */
+var asteroidsDestroyed = function(mass, asteroids) {
+    asteroids.sort((a,b)=>a-b);
+    for(let i=0;i<asteroids.length;i++){
+        if(mass>=asteroids[i]){
+            mass+=asteroids[i];
+        }else{
+            return false;
+        }
+    }
+    return true ;
+};
