c37bf1cd5966d8ba546ce38e2e0641911bbdcfca
Pratiksha Kumbhar <pratikshakumbhar0212@gmail.com>
2026-06-19 22:34:55 +0530

    Added function of Find Highest Altitude
    Added function of Find Highest Altitude

---
+++ Array/Find_Highest_Altitude.js
@@ -0,0 +1,14 @@
+var largestAltitude = function(gain) {
+    let n = gain.length;
+    let mx = 0;
+
+    for (let i = 0; i <= n; i++) {
+        let alt = 0;
+        for (let j = 0; j < i; j++) {
+            alt += gain[j];
+        }
+        mx = Math.max(mx, alt);
+    }
+
+    return mx;
+};
