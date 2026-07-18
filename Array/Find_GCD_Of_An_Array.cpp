fe47ada8c35172bbda6547e1175ae61a2a4fcf42
Pratiksha Kumbhar <pratikshakumbhar0212@gmail.com>
2026-07-18 22:57:11 +0530

    Added function of Find GCD Of an Array
    Added function of Find GCD Of an Array

---
+++ Array/Find_GCD_Of_An_Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums)
+     {
+         int n=nums.size();
+         int mn=INT_MAX;
+         int mx=INT_MIN;
+         for(int i=0;i<n;i++)
+         {
+             mn=min(mn,nums[i]);
+             mx=max(mx,nums[i]);
+         }
+         return gcd(mn,mx);
+    }
+};
