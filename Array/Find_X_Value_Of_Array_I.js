96b4c2513f7cd966114dae98b4606dfb8afa12d6
Pratiksha Kumbhar <pratikshakumbhar0212@gmail.com>
2026-09-21 22:08:38 +0530

    Added function of Find X Value Of Array I
    Added function of Find X Value Of Array I

---
+++ Array/Find_X_Value_Of_Array_I.js
@@ -0,0 +1,19 @@
+const resultArray = (A, k) => {
+    const res = new Float64Array(k);
+    let freq = new Int32Array(k);
+
+    for (let n of A) {
+        n %= k;
+        const cur = new Int32Array(k);
+        
+        cur[n] = 1;
+        for (let i = 0; i < k; i++) 
+            cur[(i * n) % k] += freq[i];        
+
+        freq = cur;
+        for (let i = 0; i < k; i++) 
+            res[i] += freq[i];        
+    }
+
+    return res;
+};
